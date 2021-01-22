41d0bf546b2e78a4527d87a9111eb1f8bb79ce03
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-22 11:28:47 +0100

    Brew formula update for gogen version 0.3

--- Formula/gogen.rb
+++ Formula/gogen.rb
@@ -5,24 +5,24 @@
 class Gogen < Formula
   desc "Simple yet powerful dynamic project generation"
   homepage "https://github.com/ksrichard/gogen"
-  version "0.2"
+  version "0.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ksrichard/gogen/releases/download/0.2/gogen_0.2_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "e6ab952497651df331d6341cd6f26033090afac176302c195c1c93f5aa84f6bb"
+    url "https://github.com/ksrichard/gogen/releases/download/0.3/gogen_0.3_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "ee860a8fe882ddbb87283f51c4c5a3549dd5064c516c1cf32a5b7174086084b3"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ksrichard/gogen/releases/download/0.2/gogen_0.2_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "00de22c05af88f8d11a53d69060043dad736ef5f24aed21bdeac06f9e27f4a35"
+    url "https://github.com/ksrichard/gogen/releases/download/0.3/gogen_0.3_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "100b70f9dff80f487c4ac62f9afbf3ffb527e698d8bbeab7e66bfd02bd88f233"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/ksrichard/gogen/releases/download/0.2/gogen_0.2_Linux_armv6.tar.gz", :using => CurlDownloadStrategy
-    sha256 "6d174c35b8660e7bed47c8159c48e26717deac1da02d464d208ef6ae593b271b"
+    url "https://github.com/ksrichard/gogen/releases/download/0.3/gogen_0.3_Linux_armv6.tar.gz", :using => CurlDownloadStrategy
+    sha256 "ce0c2772d1b4d845d7d4cd45ffe585d4d09d5e60f52b8126c7c9a768bdee5957"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/ksrichard/gogen/releases/download/0.2/gogen_0.2_Linux_arm64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "2c322902b4d446c08409832139c69d1e5272d164d267ebb9f5d51aab04e36c89"
+    url "https://github.com/ksrichard/gogen/releases/download/0.3/gogen_0.3_Linux_arm64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "13ed5792de596b8c00bba9707cd5a963f1dfa4083fac8a7f90863dde42fb8573"
   end
 
   def install
